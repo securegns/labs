986dd93a3aaf61732b97449e443f2e5642f7785f
Srikanth Grandhi <nagasrikanthgrandhi@gmail.com>
2023-07-14 13:43:27 +0100

    Update azure_http_function_bruteforceLab.py

--- azure_http_function_bruteforceLab.py
+++ azure_http_function_bruteforceLab.py
@@ -1,24 +1,58 @@
-import logging
+HTML_LOGIN_PAGE = """
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+    <form method="POST">
+        Username: <input type="text" name="username"><br>
+        Password: <input type="password" name="password"><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+"""
+
+HTML_LOGIN_FAILED = """
+<html>
+<head>
+    <title>Login Failed</title>
+</head>
+<body>
+    <h1>Login Failed</h1>
+    <p>Incorrect username or password. Please try again.</p>
+</body>
+</html>
+"""
+
+HTML_LOGIN_SUCCESS = """
+<html>
+<head>
+    <title>Login Success</title>
+</head>
+<body>
+    <h1>Login Successful</h1>
+    <p>Welcome, admin!</p>
+</body>
+</html>
+"""
+
 import azure.functions as func
 
-HTML_LOGIN_PAGE = '<!DOCTYPE html><html><head><title>Login Page</title><style>body{font-family:Arial,sans-serif;background-color:#f2f2f2;}.container{max-width:400px;margin:0 auto;padding:20px;background-color:#ffffff;border-radius:5px;box-shadow:0 0 10px rgba(0,0,0,.1);}.container h2{text-align:center;margin-bottom:20px;}.form-group{margin-bottom:15px;}.form-group label{display:block;font-weight:bold;margin-bottom:5px;}.form-group input{width:100%;padding:8px;border-radius:3px;border:1px solid #ccc;}.form-group button{width:100%;padding:8px;background-color:#4CAF50;border:none;color:#fff;cursor:pointer;border-radius:3px;}</style><script>function submitForm(){var e=document.getElementById("username").value,t=document.getElementById("password").value;fetch("",{method:"POST",body:new FormData().append("username",e).append("password",t)}).then(function(e){e.ok?console.log("Login successful"):console.log("Login failed")}).catch(function(e){console.log("Error:",e)})}</script></head><body><div class="container"><h2>Login</h2><div class="form-group"><label for="username">Username:</label><input type="text" id="username" name="username" required></div><div class="form-group"><label for="password">Password:</label><input type="password" id="password" name="password" required></div><div class="form-group"><button onclick="submitForm()">Login</button></div></div></body></html>'
-HTML_LOGIN_FAILED = '<!DOCTYPE html><html><head><title>Login Failed</title><style>body{font-family:Arial,sans-serif;background-color:#f2f2f2;}.container{max-width:400px;margin:0 auto;padding:20px;background-color:#ffffff;border-radius:5px;box-shadow:0 0 10px rgba(0,0,0,.1);}.container h2{text-align:center;margin-bottom:20px;}.error-message{color:red;text-align:center;font-weight:bold;}</style></head><body><div class="container"><h2>Login Failed</h2><p class="error-message">Invalid username or password.</p></div></body></html>'
-HTML_LOGIN_SUCESS = '<!DOCTYPE html><html><head><title>Welcome</title><style>body{font-family:Arial,sans-serif;background-color:#f2f2f2;}.container{max-width:400px;margin:0 auto;padding:20px;background-color:#ffffff;border-radius:5px;box-shadow:0 0 10px rgba(0,0,0,.1);}.container h2{text-align:center;margin-bottom:20px;}.success-message{color:green;text-align:center;font-weight:bold;}</style></head><body><div class="container"><h2>Login Successful</h2><p class="success-message">Hello admin</p></div></body></html>'
-
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    method = req.method.upper()
+
     # if GET
-    if func.method == "GET":
-        html_content =  HTML_LOGIN_PAGE
+    if method == "GET":
+        html_content = HTML_LOGIN_PAGE
     # if POST
-    if func.method == "POST":
-        req_body = req.get_json()
-        username = req_body.get('username')
-        password = req_body.get('password')
-        if username == "admin" && password == "Password@123":
-            html_content =  HTML_LOGIN_SUCESS
+    elif method == "POST":
+        username = req.form.get('username')
+        password = req.form.get('password')
+        if username == "admin" and password == "Password@123":
+            html_content = HTML_LOGIN_SUCCESS
         else:
-            html_content =  HTML_LOGIN_FAILED
+            html_content = HTML_LOGIN_FAILED
 
     headers = {
         'Content-Type': 'text/html'
