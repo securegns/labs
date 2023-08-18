d7aa36434b2c3c46837aa87d551d433b93ed1b6b
Srikanth Grandhi <nagasrikanthgrandhi@gmail.com>
2023-08-18 20:52:21 +0100

    Update AzureVMsCreate.tf

--- vms/AzureVMsCreate.tf
+++ vms/AzureVMsCreate.tf
@@ -1,95 +1,132 @@
 terraform {
+  required_version = ">=0.12"
+
   required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-          skip_provider_registration = "true"
+  skip_provider_registration = "true"
+
 }
 
-variable "resource_group_name" {
-  default = "1-4f10a454-playground-sandbox"
+variable "region" {
+  type        = string
+  default     = "eastus"
 }
 
-variable "vm_count" {
-  default = 3
+resource "random_string" "random_suffix" {
+  length  = 5
+  upper   = false
+  special = false
+  number  = true
+  lower   = true
 }
 
-variable "username" {
-  default = "gns"
+data "azurerm_resource_group" "rg" {
+  name     = "1-585f1e5d-playground-sandbox"
 }
 
-variable "password" {
-  default = "Password@123"
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = "eastus"
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "myVNet"
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "myVnetgns"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "mySubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+resource "azurerm_subnet" "my_terraform_subnet" {
+  name                 = "mySubnetgns"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.1.0/24"]
 }
-
-resource "azurerm_network_interface" "nic" {
-  count               = var.vm_count
-  name                = "myNIC${count.index}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "myNicConfiguration"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip[count.index].id
-  }
-}
-
-resource "azurerm_public_ip" "public_ip" {
-  count               = var.vm_count
-  name                = "myPublicIP${count.index}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_public_ip" "my_terraform_public_ip" {
+  name                = "myPublicIPgns"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
 }
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "myNetworkSecurityGroupgns"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
-resource "azurerm_linux_virtual_machine" "vm" {
-  count                 = var.vm_count
-  name                  = "myVM${count.index}"
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = azurerm_resource_group.rg.location
-  size                  = "Standard_B1ls"
-  admin_username        = var.username
-  admin_password        = var.password
-  disable_password_authentication = false
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+resource "azurerm_network_interface" "my_terraform_nic" {
+  name                = "myNICgns"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
-  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
+  ip_configuration {
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+  }
+}
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
+}
+
+# STORAGE ACCOUNT
+resource "azurerm_storage_account" "my_storage_account" {
+  name                     = format("storgacc000%s", random_string.random_suffix.result)
+  location                 = data.azurerm_resource_group.rg.location
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
+  name                  = "myVM"
+  location              = data.azurerm_resource_group.rg.location
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  size                  = "Standard_DS1_v2"
 
   os_disk {
+    name                 = "myOsDisk"
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    storage_account_type = "Premium_LRS"
   }
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
+  }
+
+  computer_name  = "hostname"
+  admin_username = "gns"
+  admin_password = "Password@123"
+  disable_password_authentication = false
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
   }
 }
